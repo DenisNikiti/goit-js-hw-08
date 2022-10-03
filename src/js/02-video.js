479d20bdcd8d6e16930199497972ff1f22f1cd6d
DenisNikiti <den.hikitin@gmail.com>
2022-10-03 17:56:27 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
 
+
+
+const currenttime = JSON.parse(localStorage.getItem("videoplayer-current-time"))
+
+
+
+
+player.setCurrentTime(currenttime.seconds).then(function(seconds) {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            
+            break;
+
+        default:
+           
+            break;
+    }
+});
+
+
+
+
+const timeupdate =  throttle(    function (data) {
+    localStorage.setItem("videoplayer-current-time",JSON.stringify(data))
+},1000)
+    
+    
+
+
+player.on('timeupdate', timeupdate);
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from "lodash.throttle"
+const formEl = document.querySelector(".feedback-form")
+const formtTextareaEl = document.querySelector(".feedback-form textarea")
+const formEmailEl = document.querySelector(".feedback-form input[type=email]")
 
+
+const savedEmail = localStorage.getItem("feedback-form-state-email")
+const savedmessage = localStorage.getItem("feedback-form-state-text")
+
+if (savedEmail && savedmessage) {
+    formtTextareaEl.value = savedmessage
+    formEmailEl.value = savedEmail
+}
+
+
+formEl.addEventListener("submit", onFormsubmit)
+
+function onFormsubmit(evt) {
+    evt.preventDefault()
+    evt.currentTarget.reset()
+    localStorage.removeItem("feedback-form-state-email")
+    localStorage.removeItem("feedback-form-state-text")
+
+}
+
+formEmailEl.addEventListener("input", throttle(onformEmailEl,500 ))
+
+
+function onformEmailEl(evt) {
+const message = evt.currentTarget.value
+    localStorage.setItem("feedback-form-state-email",message)
+}
+
+
+formtTextareaEl.addEventListener("input", throttle(onForminpu,500))
+
+function onForminput(evt) {
+    const message = evt.currentTarget.value
+    localStorage.setItem("feedback-form-state-text",message)
+}
