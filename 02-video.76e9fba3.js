a8b5f71b2549a8446964507872d8118533d51806
DenisNikiti <105548050+DenisNikiti@users.noreply.github.com>
2022-10-08 00:23:56 +0000

    Deploying to gh-pages from @ DenisNikiti/goit-js-hw-08@a7c9585455030b13f8b505a4384ef1476223a885 🚀

--- 02-video.76e9fba3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=JSON.parse(localStorage.getItem("videoplayer-current-time"));e&&player.setCurrentTime(e.seconds).then((function(e){})).catch((function(e){e.name}));var t=throttle((function(e){localStorage.setItem("videoplayer-current-time",JSON.stringify(e))}),1e3);player.on("timeupdate",t)}();
-//# sourceMappingURL=02-video.76e9fba3.js.map
